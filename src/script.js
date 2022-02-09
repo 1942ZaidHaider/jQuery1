a77670fb1a6d3c64ee7ef499578304ae8082f305
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-09 10:35:09 +0530

    Minor fixes

--- src/script.js
+++ src/script.js
@@ -1,40 +1,54 @@
 let m_data = [];
+let m_tmp={};
 $(function () {
-  $(document).on("click", ".close", function () { //Dynamic onclick handling for close message 
-    console.log("CLSOE");
+  $(document).on("click", ".close", function () {
+    //Dynamic onclick handling for close message
+    console.log("CLOSE");
     $(this).parent("div").hide();
   });
-  //Dynamic onclick handling for delete 
-  $(document).on("click", ".delete", function(){
-    let sku=$(this).parent("td").siblings("#sku").html();
+  //Dynamic onclick handling for delete
+  $(document).on("click", ".delete", function () {
+    let sku = $(this).parent("td").siblings("#sku").html();
     del(sku);
   });
   //Dynamic onclick handling for edit
-  $(document).on("click", ".edit", function(){
+  $(document).on("click", ".edit", function () {
     $("#product_sku").val($(this).parent("td").siblings("#sku").html());
     $("#product_name").val($(this).parent("td").siblings("#name").html());
     $("#product_price").val($(this).parent("td").siblings("#price").html());
     $("#product_quantity").val($(this).parent("td").siblings("#qty").html());
     $("#product_sku").prop("readonly",true);
-    let sku=$(this).parent("td").siblings("#sku").html();
+    let sku = $(this).parent("td").siblings("#sku").html();
+    $("#add_product").val("Update Product");
     del(sku);
-  }); 
+  });
 
   //read data
   $("#add_product").click(function () {
     // add_product.onclick()
     console.log("CLICK");
     let prod = {};
-    prod.sku = $("#product_sku").val();
-    prod.name = $("#product_name").val();
-    prod.qty = $("#product_quantity").val();
-    prod.price = $("#product_price").val();
-    console.log(checkUnique(prod));
-    $("#product_sku").val("");
-    $("#product_name").val("");
-    $("#product_quantity").val("");
-    $("#product_price").val("");
-    $("#product_sku").prop("readonly",false);
+    if (!$("#product_sku").val()) {
+      error("SKU field is empty");
+    } else if (!$("#product_name").val()) {
+      error("Name field is empty");
+    } else if (isNaN(parseFloat($("#product_sku").val()))) {
+      error("SKU field not a number");
+    } else if (!isNaN(parseFloat($("#product_name").val()))) {
+      error("Name field is a number, string needed");
+    } else {
+      prod.sku = $("#product_sku").val();
+      prod.name = $("#product_name").val();
+      prod.qty = $("#product_quantity").val();
+      prod.price = $("#product_price").val();
+      console.log(checkUnique(prod));
+      $("#product_sku").val("");
+      $("#product_name").val("");
+      $("#product_quantity").val("");
+      $("#product_price").val("");
+      $("#product_sku").prop("readonly", false);
+      success();
+    }
   });
   $(".error").hide();
   $(".success").hide();
@@ -42,8 +56,7 @@ $(function () {
 function checkUnique(p) {
   console.log("chk");
   if (m_data.length < 1) {
-    $(".success").show();
-    $(".error").hide();
+    success();
     m_data.push(p);
     display();
     return true;
@@ -51,14 +64,12 @@ function checkUnique(p) {
   for (let i of m_data) {
     console.log(i);
     if (i.sku == p.sku) {
-      $(".error").show();
-      $(".success").hide();
+      error("SKU duplicate");
       return false;
     }
   }
   m_data.push(p);
-  $(".success").show();
-  $(".error").hide();
+  success();
   display();
   return true;
 }
@@ -66,21 +77,42 @@ function display() {
   console.log(m_data);
   $("#table_body").html("");
   for (i of m_data) {
-    let prodStr = `<tr><td id="sku">${i.sku}</td><td id="name">${i.name}</td><td id="qty">${i.qty}</td><td id="price">${i.price}</td><td><a href="#" class="edit">Edit</a><a href="#" class="delete">Delete</a></td></tr>`;
+    let prodStr = `<tr><td id="sku">${i.sku}</td><td id="name">${i.name}</td><td id="price">${i.price}</td><td id="qty">${i.qty}</td><td><a href="#" class="edit">Edit</a><a href="#" class="delete">Delete</a></td></tr>`;
     $("#table_body").html($("#table_body").html() + prodStr);
   }
 }
 function del(id) {
   console.log("Deleting " + id);
-  temp = [];//new array
+  temp = []; //new array
   //Adding all elements to temp except the one to be deleted
   for (let i = 0; i < m_data.length; i++) {
     if (m_data[i]["sku"] != id) {
       temp.push(m_data[i]);
     }
+    else{
+      m_tmp=m_data[i]; // saving the last deleted value
+    }
     console.log("DELETE!");
     console.log(m_data);
   }
-  m_data = temp; //Assigning temp to m_data 
+  m_data = temp; //Assigning temp to m_data
   display();
+}
+function error(str) {
+  $(".error").html(`${str}.<a href="#" class="close">X</a>`);
+  $(".error").show();
+  $(".success").hide();
+  if(!$.isEmptyObject(m_tmp)){
+    $("#product_sku").val("");
+    $("#product_name").val("");
+    $("#product_price").val("");
+    $("#product_qty").val("");
+    m_data.push(m_tmp);
+    display();
+  }
+}
+function success() {
+  $("#add_product").val("Add Product");
+  $(".error").hide();
+  $(".success").show();
 }
